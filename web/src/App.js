baad47d6dfd0ae8b83e7cf44c371ce2efe748489
iuliab <iuliaiuliabaciu@yahoo.com>
2020-11-10 20:07:42 +0200

    deleted backend from repositpry, moved web app one level up

--- web/src/App.js
+++ web/src/App.js
@@ -93,7 +93,7 @@ class App extends React.Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://glacial-brook-77305.herokuapp.com/imageurl', {
         method: 'post',
         headers: { 'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -103,7 +103,7 @@ class App extends React.Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://glacial-brook-77305.herokuapp.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json'},
             body: JSON.stringify({
--- web/src/components/signin/SignIn.js
+++ web/src/components/signin/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://glacial-brook-77305.herokuapp.com/signin', {
       method: 'post',
       headers: { 'Content-Type': 'application/json'},
       body: JSON.stringify({
